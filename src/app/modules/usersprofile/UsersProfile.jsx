02057919295188f0d1eb9a78115588d1261ab690
jixianmin <khm9331@naver.com>
2022-08-27 19:12:40 +0900

    add: data

--- src/app/modules/usersprofile/UsersProfile.jsx
+++ src/app/modules/usersprofile/UsersProfile.jsx
@@ -1,32 +1,71 @@
-import React, { useEffect } from "react";
-import * as UsersProfileStyle from "./usersprofilestyle/usersprofilestyle";
-import { Badge, Descriptions } from "antd";
-import AdminSelect from "./userprofilesearch/AdminSelect";
-import UserName from "./userprofilesearch/UserName";
-import Block from "./userprofilesearch/Block";
-import CountrySelector from "./userprofilesearch/Countryselector";
-import DatePicker from "./userprofilesearch/DatePicker";
-import UserProfileList from "./userprofilelist/UserProfileList";
-import { getUserProfile } from "src/api/user/get";
+import React, { useEffect, useState, useCallback } from 'react';
+import * as UsersProfileStyle from './usersprofilestyle/usersprofilestyle';
+import { Badge, Descriptions } from 'antd';
+import AdminSelect from './userprofilesearch/AdminSelect';
+import UserName from './userprofilesearch/UserName';
+import Block from './userprofilesearch/Block';
+import CountrySelector from './userprofilesearch/Countryselector';
+import DatePicker from './userprofilesearch/DatePicker';
+import UserProfileList from './userprofilelist/UserProfileList';
+import { getUserProfile } from 'src/api/user/get';
 
 const UsersProfile = () => {
-  const token = localStorage.getItem("login-token") || "";
-  // const token =
-  //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiMmVmNzdlNjBjMWE4YTg1NTdlZDgxNDBkNjMyNDhmOTEwNTI4ZGIyZWM2YjQ1ZWU0MjJkMjJlNTYzY2UyMDE5OTEzODlmMmQ5MzhlYWM5NjIiLCJpYXQiOiIxNjYxNDE0MzQ3LjgzMTA1OCIsIm5iZiI6IjE2NjE0MTQzNDcuODMxMDYxIiwiZXhwIjoiMTY2MTUwMDc0Ny44MjE3MDkiLCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.M9iq9XSukGh5Ee65IIKScMexbPpOj1C-gpuA_X_uiuiWQ4_09JYJp1nR48NSLSSWQcpZuvcFUUEZgD1R4enNlCpdYuaoEqkByQOvhmaMnmwDCwl38uNnrqsJWTHUgwVIrT9s29yq7OTts9uBtgnQvq-78Js3jLhX20-EFa0Y3VJdQGHX2lyDPY49gVsV24bLDwPcrObYZzFyNOZs7aFkSaziSug_DP4fgcd35F9zsN98PgIjZQJR8c26Cm6P5mOkGPgzAs75-eKHqqINs8kIJxkn6KA2pwqLfPC_A1tCsf_-ylrCqbr3iyw6t4HoofwTq_xiwpV3l0_Gd4AhzjGmlJS_T1mhGsGjpsi6iJdv8ojeHcY8F6DBDmVpoRw9BCTIgPJLXtEi6S7pQ23T3Q9E9qBBYbXdDpLabTCdNzpPm3FwljVpqncCQyj7PIE4XuJ6ssp8fvgE7wBs3gabZ32UrexsO7-MBJV_DxwNNJid5pmi-FfvC-05ia2NZXLIC_G914oonOf1grndBt50BH1Dvx8b9W80RLXsxP2_AgNzc1VmKCMOM-g_UFVjQ15IevQPVcmlw4VVld_riVgJDLQlGoTvKTSEp78ICcnJmB-sUbb5Jx6K6tjevPr5Ygiv8190BGOaDotROp_CKTAKoaNxODisD1zZWSRe1ULml2D8kok";
+  // const token = localStorage.getItem('login-token') || '';
+  // // const token =
+  // //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiMmVmNzdlNjBjMWE4YTg1NTdlZDgxNDBkNjMyNDhmOTEwNTI4ZGIyZWM2YjQ1ZWU0MjJkMjJlNTYzY2UyMDE5OTEzODlmMmQ5MzhlYWM5NjIiLCJpYXQiOiIxNjYxNDE0MzQ3LjgzMTA1OCIsIm5iZiI6IjE2NjE0MTQzNDcuODMxMDYxIiwiZXhwIjoiMTY2MTUwMDc0Ny44MjE3MDkiLCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.M9iq9XSukGh5Ee65IIKScMexbPpOj1C-gpuA_X_uiuiWQ4_09JYJp1nR48NSLSSWQcpZuvcFUUEZgD1R4enNlCpdYuaoEqkByQOvhmaMnmwDCwl38uNnrqsJWTHUgwVIrT9s29yq7OTts9uBtgnQvq-78Js3jLhX20-EFa0Y3VJdQGHX2lyDPY49gVsV24bLDwPcrObYZzFyNOZs7aFkSaziSug_DP4fgcd35F9zsN98PgIjZQJR8c26Cm6P5mOkGPgzAs75-eKHqqINs8kIJxkn6KA2pwqLfPC_A1tCsf_-ylrCqbr3iyw6t4HoofwTq_xiwpV3l0_Gd4AhzjGmlJS_T1mhGsGjpsi6iJdv8ojeHcY8F6DBDmVpoRw9BCTIgPJLXtEi6S7pQ23T3Q9E9qBBYbXdDpLabTCdNzpPm3FwljVpqncCQyj7PIE4XuJ6ssp8fvgE7wBs3gabZ32UrexsO7-MBJV_DxwNNJid5pmi-FfvC-05ia2NZXLIC_G914oonOf1grndBt50BH1Dvx8b9W80RLXsxP2_AgNzc1VmKCMOM-g_UFVjQ15IevQPVcmlw4VVld_riVgJDLQlGoTvKTSEp78ICcnJmB-sUbb5Jx6K6tjevPr5Ygiv8190BGOaDotROp_CKTAKoaNxODisD1zZWSRe1ULml2D8kok";
+  // useEffect(() => {
+  //   fetch(
+  //     'https://api.game.tiadev.net/api/user?page=1&pageLimit=10&pagination=Y&order=&nickname=bot&sort=',
+  //     {
+  //       method: 'Get',
+  //       headers: {
+  //         Authorization: 'Bearer ' + token,
+  //       },
+  //     }
+  //   )
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       console.log(result);
+  //     });
+  // }, []);
+
+  const [filters, setFilters] = useState({
+    level: '',
+    name: '',
+    nickname: '',
+    email: '',
+    hp: '',
+    code: '',
+    nation: '',
+    start_at: '',
+    end_at: '',
+    blind: '',
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+
+  const [buckets, setBuckets] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    const payload = {
+      ...filters,
+      page: currentPage,
+      perPage: 20,
+      pagination: 'Y',
+    };
+
+    await getUserProfile(payload)
+      .then(({ data }) => {
+        if (data.success) {
+          setBuckets(data.data.data);
+          setTotalPage(data.data.total);
+        }
+      })
+      .catch((e) => {});
+  }, [filters]);
+
   useEffect(() => {
-    fetch(
-      "https://api.game.tiadev.net/api/user?page=1&pageLimit=10&pagination=Y&order=&nickname=bot&sort=",
-      {
-        method: "Get",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-      });
+    fetchData();
   }, []);
 
   return (
@@ -46,31 +85,31 @@ const UsersProfile = () => {
           </UsersProfileStyle.UsersProfileFindEditorFirstBox>
           <UsersProfileStyle.UsersProfileFindEditorSecondBox>
             <Descriptions bordered span={3}>
-              <Descriptions.Item label="권한">
+              <Descriptions.Item label='권한'>
                 <AdminSelect />
               </Descriptions.Item>
-              <Descriptions.Item label="이름">
+              <Descriptions.Item label='이름'>
                 <UserName />
               </Descriptions.Item>
-              <Descriptions.Item label="닉네임">
+              <Descriptions.Item label='닉네임'>
                 <UserName />
               </Descriptions.Item>
-              <Descriptions.Item label="이메일">
+              <Descriptions.Item label='이메일'>
                 <UserName />
               </Descriptions.Item>
-              <Descriptions.Item label="전화번호">
+              <Descriptions.Item label='전화번호'>
                 <UserName />
               </Descriptions.Item>
-              <Descriptions.Item label="추천코드">
+              <Descriptions.Item label='추천코드'>
                 <UserName />
               </Descriptions.Item>
-              <Descriptions.Item label="국가">
+              <Descriptions.Item label='국가'>
                 <CountrySelector />
               </Descriptions.Item>
-              <Descriptions.Item label="차단여부">
+              <Descriptions.Item label='차단여부'>
                 <Block />
               </Descriptions.Item>
-              <Descriptions.Item label="가입일">
+              <Descriptions.Item label='가입일'>
                 <DatePicker />
               </Descriptions.Item>
             </Descriptions>
@@ -84,9 +123,7 @@ const UsersProfile = () => {
             </UsersProfileStyle.UsersProfileFindEditorFirstBoxRight>
           </UsersProfileStyle.UsersProfileFindEditorThirdBox>
         </UsersProfileStyle.UsersProfileFindEditor>
-        <UserProfileList
-          style={{ margin: "10px", backgroundColor: "red", color: "red" }}
-        />
+        <UserProfileList buckets={buckets} currentPage={currentPage} />
       </UsersProfileStyle.UsersProfileWrapper>
     </>
   );
--- src/app/modules/usersprofile/userprofilelist/UserProfileList.jsx
+++ src/app/modules/usersprofile/userprofilelist/UserProfileList.jsx
@@ -1,73 +1,74 @@
-import { Table } from "antd";
-import qs from "qs";
-import React, { useEffect, useState } from "react";
+import { Table } from 'antd';
+import qs from 'qs';
+import React, { useEffect, useState } from 'react';
 const columns = [
   {
-    title: "ID",
-    dataIndex: "id",
+    title: 'ID',
+    dataIndex: 'id',
     // width: "20%",
-    width: "70px",
+    width: '70px',
   },
   {
-    title: "이메일",
-    dataIndex: "email",
+    title: '이메일',
+    dataIndex: 'email',
     sorter: true,
-    width: "249px",
+    width: '249px',
   },
   {
-    title: "닉네임",
-    dataIndex: "name",
+    title: '닉네임',
+    // dataIndex: 'nickname',
+    dataIndex: 'nickname',
     sorter: true,
-    render: (name) => `${name.first} ${name.last}`,
-    width: "129px",
+    // render: (name) => `${name.first} ${name.last}`,
+    width: '129px',
   },
   {
-    title: "국가",
-    dataIndex: "country",
+    title: '국가',
+    dataIndex: 'nation',
     sorter: true,
-    width: "129px",
+    width: '129px',
   },
   {
-    title: "권한",
-    dataIndex: "authority",
+    title: '권한',
+    dataIndex: 'level',
     sorter: true,
-    width: "89px",
+    width: '89px',
   },
   {
-    title: "블루브릭",
-    dataIndex: "bluebrick",
+    title: '블루브릭',
+    dataIndex: 'blue_point',
     sorter: true,
-    width: "119px",
+    width: '119px',
   },
   {
-    title: "레드브릭",
-    dataIndex: "redbrick",
+    title: '레드브릭',
+    dataIndex: 'red_point',
     sorter: true,
-    width: "119px",
+    width: '119px',
   },
   {
-    title: "T-브릭",
-    dataIndex: "t-brick",
+    title: 'T-브릭',
+    dataIndex: 't_point',
     sorter: true,
-    width: "119px",
+    width: '119px',
   },
   {
-    title: "가입일",
-    dataIndex: "Member since",
+    title: '가입일',
+    dataIndex: 'created_at',
     sorter: true,
-    width: "119px",
+    width: '119px',
   },
   {
-    title: "최근접속일",
-    dataIndex: "recently accessed",
+    title: '최근접속일',
+    dataIndex: 'updated_at',
     sorter: true,
-    width: "119px",
+    width: '119px',
   },
   {
-    title: "기능",
-    dataIndex: "function",
+    title: '기능',
+    dataIndex: 'function',
     sorter: true,
-    width: "250px",
+    width: '250px',
   },
 ];
 
@@ -77,51 +78,56 @@ const getRandomuserParams = (params) => ({
   ...params,
 });
 
-const App = () => {
+const App = ({ buckets, currentPage }) => {
+  console.log(buckets);
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
   const [pagination, setPagination] = useState({
     current: 1,
-    pageSize: 10,
+    pageSize: 20,
   });
+  ////////////////////
+  //console.log(buckets);
+  //////////////
+  const fetchData = (params = {}) => {
+    setLoading(true);
+    fetch(
+      `https://randomuser.me/api?${qs.stringify(getRandomuserParams(params))}`
+    )
+      .then((res) => res.json())
+      .then(({ results }) => {
+        console.log(results);
+        setData(results);
+        setLoading(false);
+        setPagination({
+          ...params.pagination,
+          total: 200, // 200 is mock data, you should read it from server
+          // total: data.totalCount,
+        });
+      });
+  };
 
-  // const fetchData = (params = {}) => {
-  //   setLoading(true);
-  //   fetch(
-  //     `https://randomuser.me/api?${qs.stringify(getRandomuserParams(params))}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then(({ results }) => {
-  //       setData(results);
-  //       setLoading(false);
-  //       setPagination({
-  //         ...params.pagination,
-  //         total: 200, // 200 is mock data, you should read it from server
-  //         // total: data.totalCount,
-  //       });
-  //     });
-  // // };
+  useEffect(() => {
+    console.log(pagination);
+    fetchData({
+      pagination,
+    });
+  }, []);
 
-  // useEffect(() => {
-  //   fetchData({
-  //     pagination,
-  //   });
-  // }, []);
-
-  // const handleTableChange = (newPagination, filters, sorter) => {
-  //   fetchData({
-  //     sortField: sorter.field,
-  //     sortOrder: sorter.order,
-  //     pagination: newPagination,
-  //     ...filters,
-  //   });
-  // };
+  const handleTableChange = (newPagination, filters, sorter) => {
+    fetchData({
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      pagination: newPagination,
+      ...filters,
+    });
+  };
 
   return (
     <Table
       columns={columns}
-      rowKey={(record) => record.login.uuid}
-      dataSource={data}
+      rowKey={(record) => record.id}
+      dataSource={buckets}
       //   pagination={pagination}
       pagination={pagination}
       loading={loading}
