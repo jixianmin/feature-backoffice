25d6927804970b6dc735901628628260f17df803
길현민 <jxm9331@gmail.com>
2022-08-30 16:00:23 +0900

    Add: codereview

--- src/app/modules/pre-reservation/statusreservation/StatusEcharts.jsx
+++ src/app/modules/pre-reservation/statusreservation/StatusEcharts.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactECharts from 'echarts-for-react';
-//import * as echarts from "echarts/core";
+import React from "react";
+import ReactECharts from "echarts-for-react";
 
 const Page = ({ buckets, allBuckets }) => {
   const bucketsdate = () => {
@@ -18,44 +17,105 @@ const Page = ({ buckets, allBuckets }) => {
   const options = {
     grid: { top: 8, right: 8, bottom: 24, left: 36 },
     xAxis: {
-      type: 'category',
-      //data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-      //data: [buckets.date],
-      //data: [buckets.map((res) => res.date)],
+      type: "category",
       data: bucketsdate(),
     },
     yAxis: {
-      type: 'value',
+      type: "value",
     },
     series: [
       {
-        // data: [
-        //   820, 932, 901, 934, 1290, 1330, 1320, 820, 932, 901, 934, 1290, 1330,
-        //   1320,
-        // ],
-        //data: [buckets.count],
-        //data: [buckets.map((res) => res.count)],
-        type: 'line',
-        stack: 'Total',
+        type: "line",
+        stack: "Total",
         data: bucketscount(),
       },
     ],
     tooltip: {
-      trigger: 'axis',
+      trigger: "axis",
     },
   };
 
   //console.log(allBuckets);
-  console.log(allBuckets.daily_channel_user_count); //{push:Array(13), fb:Array(13)} 객체 리터럴(object)
+  //console.log(allBuckets.daily_channel_user_count); //{push:Array(13), fb:Array(13)} 객체 리터럴(object)
   //console.log(Array.isArray(allBuckets.daily_channel_user_count)); //boolean 맨처음 true 채널 넣으면 false
   //  for...in문 사용하기
   const allBucketsForIn = () => {
     if (!Array.isArray(allBuckets.daily_channel_user_count)) {
+      const allBucketscount = []; //['push', 'popup', 'fb']
+      const allBucketData = {}; //{push: Array(13), popup: Array(13)}
+
       for (const property in allBuckets.daily_channel_user_count) {
-        console.log(
-          `${property}: ${allBuckets.daily_channel_user_count[property]}`
-        );
+        allBucketscount.push(property);
       }
+      for (const element of allBucketscount) {
+        allBucketData[element] = allBuckets.daily_channel_user_count[element];
+      }
+
+      const bucketsdate = () => {
+        //(13)['','','','','']
+        const arr = [];
+        if (Array.isArray(allBucketData[allBucketscount[0]])) {
+          allBucketData[allBucketscount[0]].map((res) => arr.push(res.date));
+        }
+        return arr;
+      };
+
+      // const bucketscount = () => {
+      //   const arr = [];
+      //   if (Array.isArray(allBucketData[allBucketscount[0]])) {
+      //     allBucketData.res.map((result) => arr.push(result.completed));
+      //   }
+      //   return arr;
+      // };
+
+      const seriesALLData = () => {
+        const seriesData = [];
+        allBucketscount.map((res) => {
+          const bucketscount = () => {
+            const arr = [];
+            if (Array.isArray(allBucketData[allBucketscount[0]])) {
+              allBucketData.res.map((result) => arr.push(result.completed));
+            }
+            return arr;
+          };
+          seriesData.push({
+            name: res,
+            type: "line",
+            stack: "Total",
+            data: bucketscount2(),
+          });
+          //seriesData.push(allBucketData[res]);
+        });
+        return seriesData;
+      };
+
+      return seriesALLData();
+      //allBuckets.daily_channel_user_count[allBucketscount[0]] === allBucketData[allBucketscount[0]]
+      const makeoptions = {
+        legend: {
+          data: allBucketscount,
+        },
+        grid: { top: 8, right: 8, bottom: 24, left: 36 },
+        xAxis: {
+          type: "category",
+          data: bucketsdate(),
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            name: "push",
+            type: "line",
+            stack: "Total",
+            data: bucketscount2(),
+          },
+        ],
+        tooltip: {
+          trigger: "axis",
+        },
+      };
+      return makeoptions;
     }
   };
 
@@ -81,26 +141,26 @@ const Page = ({ buckets, allBuckets }) => {
 
   const options2 = {
     legend: {
-      data: ['push'],
+      data: ["push"],
     },
     grid: { top: 8, right: 8, bottom: 24, left: 36 },
     xAxis: {
-      type: 'category',
+      type: "category",
       data: bucketsdate2(),
     },
     yAxis: {
-      type: 'value',
+      type: "value",
     },
     series: [
       {
-        name: 'push',
-        type: 'line',
-        stack: 'Total',
+        name: "push",
+        type: "line",
+        stack: "Total",
         data: bucketscount2(),
       },
     ],
     tooltip: {
-      trigger: 'axis',
+      trigger: "axis",
     },
   };
 
